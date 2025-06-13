99cbef0e49932f157f8fbfaa14554a20b38214c7
hyeonaa02 <jeonglucy00@gmail.com>
2025-06-13 16:32:37 +0900

    refactor : 상단바에서 유저정보 axios로 요청

--- src/main/java/com/oopsw/clario/controller/member/MemberApiController.java
+++ src/main/java/com/oopsw/clario/controller/member/MemberApiController.java
@@ -2,12 +2,14 @@ package com.oopsw.clario.controller.member;
 
 
 import com.oopsw.clario.config.auth.CustomOAuth2User;
+import com.oopsw.clario.domain.member.Member;
 import com.oopsw.clario.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,6 +20,33 @@ public class MemberApiController {
 
     public MemberApiController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    //헤더 사이드바에서 유저 정보 가져오기
+    @GetMapping("/api/userinfo")
+    public Map<String, Object> getUserInfo(@AuthenticationPrincipal CustomOAuth2User user) {
+        Map<String, Object> result = new HashMap<>();
+
+        if (user == null) {
+            result.put("authenticated", false);
+            return result;
+        }
+
+        String email = user.getEmail();
+        Member member = memberService.findNullableMemberByEmail(email);
+
+        if (member == null) {
+            // 이 경우는 회원가입 안 됐거나 DB 문제이므로, 오류 처리하거나 강제 로그아웃 등 조치 필요
+            throw new RuntimeException("회원 정보가 존재하지 않습니다. 로그인 세션을 재확인 해주세요.");
+        }
+
+        // 정상적으로 회원가입된 유저의 정보 반환
+        result.put("authenticated", true);
+        result.put("name", member.getName());
+        result.put("email", member.getEmail());
+        result.put("phonenum", member.getPhonenum());
+        result.put("profile", user.getPhoto());
+
+        return result;
     }
 
     @PostMapping("/reset-password")
